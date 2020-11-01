209128cde28837fdd1656b6cc1c154fad82a8819
StreamMao <maosijun1995@gmail.com>
2020-11-01 14:08:57 -0500

    React router: BrowserRouter, NavLink, Switch, Route; Ant Design; less-loader; CSS Module;

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
+import Router from './router';
+import styles from './App.module.less';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu className={styles.menu} theme="dark">
+        <Menu.Item><NavLink to="/">Home</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/Login">Login</NavLink></Menu.Item>
+        <Menu.Item><NavLink to="/Register">Register</NavLink></Menu.Item>
+      </Menu>
+      <Router></Router>
+    </BrowserRouter>
   );
 }
 
